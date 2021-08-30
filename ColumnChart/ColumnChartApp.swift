620cc975d52f64ff522e3ad5d047e82138087d73
Simon Bogutzky <sbogutzky@oelschlaeger.de>
2021-08-30 13:26:25 +0200

    Create SwiftUI project

---
+++ ColumnChart/ColumnChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColumnChartApp.swift
+//  ColumnChart
+//
+//  Created by Dr. Simon Bogutzky.
+//
+
+import SwiftUI
+
+@main
+struct ColumnChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColumnChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColumnChart
+//
+//  Created by Dr. Simon Bogutzky.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
